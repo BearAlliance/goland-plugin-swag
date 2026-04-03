e1b3e7184cb1b84b2dd7bcb8002549c610d0217e
Nick Cacace <NickCacace@gmail.com>
2026-04-03 13:58:48 -0400

    ci: automatically publish plugin with CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.github.bearalliance"
-version = "1.0.0"
+version = System.getenv("RELEASE_VERSION") ?: "1.0.0"
 
 repositories {
     mavenCentral()
@@ -28,5 +28,9 @@ tasks {
 
     buildSearchableOptions {
         enabled = false
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
     }
 }
